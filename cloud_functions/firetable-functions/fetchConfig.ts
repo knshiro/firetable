ed35055341321ddd0e0482da02efdb5fb6d01edb
Ugo Bataillard <ugopublic@bataillard.me>
2020-03-10 14:42:58 +0900

    Fix bin

--- cloud_functions/firetable-functions/fetchConfig.ts
+++ cloud_functions/firetable-functions/fetchConfig.ts
@@ -1,15 +1,23 @@
+#!/usr/bin/env node
+
 import * as fs from "fs";
 // Initialize Firebase Admin
 import * as admin from "firebase-admin";
 // Initialize Firebase Admin
 
-if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
-  throw new Error(
-    "Specify GOOGLE_APPLICATION_CREDENTIALS env variable with path to service-account.js"
+const credentialsPath = process.env.GOOGLE_APPLICATION_CREDENTIALS || "./firebase-credentials.json";
+
+let serviceAccount;
+try {
+  serviceAccount = JSON.parse(fs.readFileSync(credentialsPath, 'utf8'));
+}
+catch {
+  console.error(
+    `Couldn't find ${process.cwd()}/${credentialsPath} , set GOOGLE_APPLICATION_CREDENTIALS env variable with path to service-account.js`
   );
+  process.exit(1);
 }
 
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 console.log(`Running on ${serviceAccount.project_id}`);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -17,28 +25,25 @@ admin.initializeApp({
 });
 const db = admin.firestore();
 
-const docConfig2json = async (docPath: string, jsonPath: string) => {
-  const doc = await db.doc(docPath).get();
-  const data = doc.data();
-  const jsonData = JSON.stringify(data ? data.config : "");
-  fs.writeFileSync(jsonPath, jsonData);
+const fetchAllDocConfigs = async (...docPaths: string[]) => {
+  const docRefs = docPaths.map(docPath => db.doc(docPath))
+  const docs = await db.getAll(...docRefs)
+  return docs.map(doc => doc.data()?.config);
 };
 
 // Initialize Cloud Firestore Database
+const docConfig2json = (path, data) => {
+  const jsonData = JSON.stringify(data);
+  fs.writeFileSync(path, jsonData);
+}
 
 const main = async () => {
-  await docConfig2json(
+  const [history, algolia, sync] = await fetchAllDocConfigs(
     "_FIRETABLE_/_SETTINGS_/_CONFIG_/_HISTORY_",
-    "./src/history/config.json"
-  );
-  await docConfig2json(
     "_FIRETABLE_/_SETTINGS_/_CONFIG_/_ALGOLIA_",
-    "./src/algolia/config.json"
-  );
-  await docConfig2json(
     "_FIRETABLE_/_SETTINGS_/_CONFIG_/_COLLECTION_SYNC_",
-    "./src/collectionSync/config.json"
-  );
+  )
+  docConfig2json("firetableconfig.json", { history, algolia, sync} )
   return true;
 };
 
